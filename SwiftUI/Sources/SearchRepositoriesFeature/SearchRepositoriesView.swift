acbd119c0c9b3b274f249f3733f1196dfaddb973
tomoya <cj.0s0ttw.nsm4@gmail.com>
2024-07-04 04:07:31 +0900

    検索バーの見た目だけ作成

--- SwiftUI/Sources/SearchRepositoriesFeature/SearchRepositoriesView.swift
+++ SwiftUI/Sources/SearchRepositoriesFeature/SearchRepositoriesView.swift
@@ -4,6 +4,8 @@ import RepositoryDetailFeature
 
 public struct SearchRepositoriesView: View {
   let store: StoreOf<SearchRepositoriesReducer>
+  @State var editing: Bool = false
+  @FocusState var focus:Bool
   
   struct ViewState: Equatable {
     @BindingViewState var query: String
@@ -24,6 +26,53 @@ public struct SearchRepositoriesView: View {
   public var body: some View {
     NavigationStackStore(store.scope(state: \.path, action: \.path)) {
       WithViewStore(store, observe: ViewState.init(store:)) { viewStore in
+        VStack {
+          
+          HStack {
+            ZStack {
+              RoundedRectangle(cornerRadius: 8)
+                .fill(Color(red: 239 / 255,
+                            green: 239 / 255,
+                            blue: 241 / 255))
+                .frame(height: 36)
+              
+              HStack(spacing: 6) {
+                Button {
+                  //Todo：検索処理
+                } label: {
+                  Image(systemName: "magnifyingglass")
+                    .foregroundColor(.gray)
+                    .padding([.leading, .trailing], 8)
+                }
+                
+                TextField("Search", text: viewStore.$query)
+                  .focused(self.$focus)
+                
+                if !viewStore.$query.wrappedValue.isEmpty {
+                  Button {
+                    //Todo：$queryを空にする処理
+                  } label: {
+                    Image(systemName: "xmark.circle.fill")
+                      .foregroundColor(.gray)
+                  }
+                  .padding(.trailing, 6)
+                }
+              }
+            }
+            .padding(.top, self.focus ? 0 : 40)
+
+            if self.focus {
+              Button(action: {
+                self.focus = false
+                //Todo：$queryを空にする処理、フォーカスを外す
+              }, label: {
+                Text("Cancel")
+              })
+            }
+          }
+        }
+        .padding(.horizontal)
+        
         List {
           Toggle(isOn: viewStore.$showFavoritesOnly) {
             Text("Favorites Only")
@@ -53,7 +102,6 @@ public struct SearchRepositoriesView: View {
               .frame(maxWidth: .infinity)
           }
         }
-        .searchable(text: viewStore.$query)
       }
     } destination: {
       RepositoryDetailView(store: $0)
